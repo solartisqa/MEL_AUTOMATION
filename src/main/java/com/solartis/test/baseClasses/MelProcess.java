2d0a43f1317062b298f8baa91e1f797c6e7da4d2
Sharath-Solartis <sharath_n@solartis.net>
2018-04-19 16:36:35 +0530

    main class added

--- src/main/java/com/solartis/test/baseClasses/MelProcess.java
+++ src/main/java/com/solartis/test/baseClasses/MelProcess.java
@@ -368,19 +368,4 @@ public class MelProcess extends MelProcessBaseClass
 		return LookupValue;		
 		
 	}
-		
-	public static void main(String args[]) throws DatabaseException, PropertiesHandleException, MacroException, SQLException
-	{
-		PropertiesHandle configFile = new PropertiesHandle("com.mysql.jdbc.Driver","jdbc:mysql://192.168.84.225:3700/JmeterDB-STARR_ISO?useSSL=false","root","redhat");
-		DatabaseOperation.ConnectionSetup(configFile);
-		MelProcess processmel = new MelProcess(configFile);
-		processmel.generateExpectedMel();
-		//processmel.Comparison("MelActual", "MelActual_copy");
-		 Calendar calendar = Calendar.getInstance();
-
-		    int lastDate = calendar.getActualMaximum(Calendar.DATE);
-
-		    System.out.println("Date     : " + calendar.getTime());
-		    System.out.println("Last Date: " + lastDate);
-	}
 }
---
+++ src/main/java/com/solartis/test/melAutomation/MainClass.java
@@ -0,0 +1,29 @@
+package com.solartis.test.melAutomation;
+
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import com.solartis.test.baseClasses.MelProcess;
+import com.solartis.test.configuration.PropertiesHandle;
+import com.solartis.test.exception.DatabaseException;
+import com.solartis.test.exception.MacroException;
+import com.solartis.test.exception.PropertiesHandleException;
+import com.solartis.test.util.common.DatabaseOperation;
+
+public class MainClass 
+{
+	public static void main(String args[]) throws DatabaseException, PropertiesHandleException, MacroException, SQLException
+	{
+		PropertiesHandle configFile = new PropertiesHandle("com.mysql.jdbc.Driver","jdbc:mysql://192.168.84.225:3700/JmeterDB-STARR_ISO?useSSL=false","root","redhat");
+		DatabaseOperation.ConnectionSetup(configFile);
+		MelProcess processmel = new MelProcess(configFile);
+		processmel.generateExpectedMel();
+		//processmel.Comparison("MelActual", "MelActual_copy");
+		 Calendar calendar = Calendar.getInstance();
+
+		    int lastDate = calendar.getActualMaximum(Calendar.DATE);
+
+		    System.out.println("Date     : " + calendar.getTime());
+		    System.out.println("Last Date: " + lastDate);
+	}
+}
