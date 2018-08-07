3827e76789cd206315b07170e205a6bcb73bdcdf
solartisqa <sasirekha_a@solartis.com>
2018-08-07 10:29:18 +0530

    generalize

--- src/main/java/com/solartis/test/melAutomation/MainClass.java
+++ src/main/java/com/solartis/test/melAutomation/MainClass.java
@@ -19,14 +19,13 @@ import com.solartis.test.util.common.DatabaseOperation;
 
 public class MainClass 
 {
-	@SuppressWarnings("unused")
 	public static void main(String args[]) throws DatabaseException, PropertiesHandleException, MacroException, SQLException, FileNotFoundException, POIException, IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException
 	{
 		//PropertiesHandle configFile = new PropertiesHandle("com.mysql.jdbc.Driver","jdbc:mysql://192.168.84.225:3700/JmeterDB-STARR_ISO?useSSL=false","root","redhat");
 		PropertiesHandle configFile = new PropertiesHandle(System.getProperty("JDBC_DRIVER"),System.getProperty("DB_URL")+"?useSSL=false",System.getProperty("USER"),System.getProperty("password"));
 		DatabaseOperation.ConnectionSetup(configFile);
 		
-		Class<?> cl = Class.forName("com.solartis.test.product."+"StarrGL"+".MelProcess");
+		Class<?> cl = Class.forName("com.solartis.test.product."+System.getProperty("Project")+".MelProcess");
 		Constructor<?> cons = cl.getConstructor(com.solartis.test.configuration.PropertiesHandle.class);
 		MelProcessBaseClass api =  (MelProcessBaseClass) cons.newInstance(configFile);
 		
